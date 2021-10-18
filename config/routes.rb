35d915fe30504132ddccc96405d43d22d1d49385
wataridmm <r.watari.watari@gmail.com>
2021-10-18 13:12:23 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords,], controllers: {
-  registrations: "customers/registrations",
-  sessions: 'customers/sessions'
+  registrations: "customer/registrations",
+  sessions: 'customer/sessions'
 }
+# 5.6行目↑コントローラーはどこのを参照しているか
 
 # 管理者用
 # URL /admin/sign_in ...
