6fed91f2af78a9e2dfe367b00ddde83974195da5
wataridmm <r.watari.watari@gmail.com>
2021-10-19 07:09:41 +0000

    add customer culum

--- db/migrate/20211018094009_devise_create_customers.rb
+++ db/migrate/20211018094009_devise_create_customers.rb
@@ -6,6 +6,18 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      
+      
+      t.string :last_name, null: false, default: ""
+      t.string :first_name, null: false, default: ""
+      t.string :last_name_kana, null: false, default: ""
+      t.string :first_name_kana, null: false, default: ""
+      t.string :postal_code, null: false, default: ""
+      t.string :address, null: false, default: ""
+      t.string :telephone_number, null: false, default: ""
+      t.boolean :is_deleted, null: false, default: "FALSE"
+    
+      
 
       ## Recoverable
       t.string   :reset_password_token
