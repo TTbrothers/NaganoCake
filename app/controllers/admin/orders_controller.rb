c90a0028cc044145741c179110dfcbf741d78177
toi0621 <burningup24@gmail.com>
2021-10-23 10:59:37 +0000

    add customeer view

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,33 @@
 class Admin::OrdersController < ApplicationController
+
+before_action :authenticate_admin!
+
+def index
+ @orders = Order.all.page(params[:page]).per(10)
+
+end
+
+def show
+ @order = Order.find(params[:id])
+ @order_details = @order.order_details
+
+end
+
+def show
+ @order = Order.find(params[:id])
+ if @order.update(order_params)
+  flash[:success] = "注文ステータスを変更しました"
+  redirect_to admin_order_path(@order)
+ else
+  render "show"
+ end
+
+end
+
+private
+def order_params
+ params.require(:order).permit(:order_status)
+
+end
+
 end
