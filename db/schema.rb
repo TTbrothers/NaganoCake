986678f5adb9cfbbf372dbb132b29b98602df6e2
toi0621 <burningup24@gmail.com>
2021-10-24 04:28:50 +0000

    add admin view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20211019075201) do
 
   create_table "addresses", force: :cascade do |t|
@@ -62,6 +61,19 @@ ActiveRecord::Schema.define(version: 20211019075201) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "items", force: :cascade do |t|
+    t.integer  "genre_id"
+    t.string   "name"
+    t.string   "image_id"
+    t.text     "introduction"
+    t.integer  "price"
+    t.boolean  "is_active"
+    t.datetime "create_at"
+    t.datetime "update_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
   create_table "jenres", force: :cascade do |t|
     t.string   "name",       default: "", null: false
     t.datetime "created_at",              null: false
@@ -77,19 +89,18 @@ ActiveRecord::Schema.define(version: 20211019075201) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
-  
-  create_table "items", force: :cascade do |t|
-    t.integer  "genre_id"
-    t.string   "name"
-    t.string   "image_id"
-    t.text     "introduction"
-    t.integer  "price"
-    t.boolean  "is_active"
-    t.datetime "create_at"
-    t.datetime "update_at"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
 
+  create_table "orders", force: :cascade do |t|
+    t.integer  "customer_id",                null: false
+    t.string   "postal_code",                null: false
+    t.string   "address",                    null: false
+    t.string   "name",                       null: false
+    t.integer  "shipping_cost",              null: false
+    t.integer  "total_payment",              null: false
+    t.integer  "payment_method", default: 0, null: false
+    t.integer  "status",         default: 0, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
