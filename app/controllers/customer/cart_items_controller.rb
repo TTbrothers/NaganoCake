a97383f9f2906d8ebe408b45b21bf54422909e5c
niwanekochan <pooonda.0607@gmail.com>
2021-10-26 09:20:45 +0000

    カート商品追加まで

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,2 +1,24 @@
 class Customer::CartItemsController < ApplicationController
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to customer_cart_items_path
+  end
+
+  def index
+    @cart_items = current_customer.cart_items
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity, :item_id, :customer_id)
+  end
 end
--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -6,6 +6,7 @@ class Customer::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
   def search
