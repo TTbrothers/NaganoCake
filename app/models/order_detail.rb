74270554fb3793afe88142491f11b7fb0e323c5a
toi0621 <burningup24@gmail.com>
2021-10-27 06:52:46 +0000

    order_detail item

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,7 +1,7 @@
 class OrderDetail < ApplicationRecord
 
  belongs_to :order
- belongs_to :items
+ belongs_to :item
 
  validates :item_id, :order_id, :amount, :price, presence: true
  validates :price, :amount, numericality: { only_integer: true }
