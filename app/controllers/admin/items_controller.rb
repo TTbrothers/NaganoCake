79ff5697157f8f40f80b7eb7fcb2a51255de0e09
niwanekochan <pooonda.0607@gmail.com>
2021-10-28 11:58:38 +0000

    code修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,8 +5,12 @@ class Admin::ItemsController < ApplicationController
 
   def create
     item = Item.new(item_params)
-    item.save
-    redirect_to admin_item_path(item.id)
+    if item.save
+      redirect_to admin_item_path(item.id)
+    else
+      @item = Item.new
+      render :new
+    end
   end
 
   def index
@@ -20,8 +24,12 @@ class Admin::ItemsController < ApplicationController
 
   def update
     item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to admin_item_path(item.id)
+    if item.update(item_params)
+      redirect_to admin_item_path(item.id)
+    else
+      @item = Item.find(params[:id])
+      render :edit
+    end
   end
 
   def show
